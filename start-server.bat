78b61209c3fab4d8bb4d44221aa48dfb5bc18f74
Jaime Miranda <Jaime.L.MirandaRamirez@outlook.com>
2026-05-05 22:48:45 -0500

    Added .bat and .command file to launch server on double-click on Windows and macOS, respectively

---
+++ start-server.bat
@@ -0,0 +1,8 @@
+@echo off
+title Gies P2P Chat
+echo Starting Gies P2P Chat Server...
+:: Navigate to the folder where this batch file lives
+cd /d "%~dp0"
+:: Start the server
+node server.cjs
+pause
